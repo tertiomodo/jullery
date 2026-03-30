96cce841e65acb9bb7e0007ae3e5a6a8eb1b40c9
tertiomodo <itertiomodo@gmail.com>
2026-03-30 15:36:37 +0500

    Fixed remark

--- src/@modules/Works/components/WorksTitle/WorksTitle.tsx
+++ src/@modules/Works/components/WorksTitle/WorksTitle.tsx
@@ -8,7 +8,7 @@ const WorksTitle: React.FC = () => {
     >
       <div className="container">
         <div className={styles.wrapper}>
-          <h2 className={styles.title}>My lovely works</h2>
+          <h2 className={styles.title}>My favorite works</h2>
           <p className={styles.subtitle}>
             Here are moments that hold the greatest value for me. Each image is
             a small part of my life, captured in a frame
